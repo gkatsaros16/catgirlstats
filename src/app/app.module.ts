41c8478c878e559e6adc4e94ff31e06707a14d71
gkatsaros16 <gkatsaros16@gmail.com>
2021-10-31 22:06:43 -0400

    lots of goodies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { SortPipe } from './pipes/sort.pipe'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GraphQLModule } from './graphql.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SortPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GraphQLModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [SortPipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

cf64bad8e0c183a6caf14f69a5aa3d0c62a64ea1
gkatsaros16 <gkatsaros16@gmail.com>
2021-11-03 21:00:35 -0400

    copy click address

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,21 @@ import { nftadeContextService } from './services/nftrade-context.service'
 })
 
 export class AppComponent {
+  isCopied = false;
   constructor(private nftContext: nftadeContextService) {}
+
+  copyAddress() {
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = "0xa119a6F239fEF7b429Fd89C43f0BF227C5C831Cb";
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+    this.isCopied = true;
+  }
 }
